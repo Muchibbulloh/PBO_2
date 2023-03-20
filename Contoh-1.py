d899f3da479ed543e9e357d03b4a5fa45d6ebda5
Muhamad Muchibbulloh <127671970+Muchibbulloh@users.noreply.github.com>
2023-03-20 09:37:52 +0700

    Delete Contoh-1.py

--- Contoh-1.py
+++
@@ -1,8 +0,0 @@
-class Motor:
-    def __init__(self, merk, warna):
-        self.merk = merk
-        self.warna = warna
-    def info(self):
-        print(f"Motor {self.merk} berwarna {self.warna}")
-MotorA = Motor("Supra", "Biru")
-MotorA.info() 
